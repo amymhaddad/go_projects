20b66808436da13b497cdd929f72ae50c3131d77
Amy M Haddad <amy.m.haddad@gmail.com>
2021-06-20 08:47:05 -0400

    Refactored division test cases

--- project_1_go_fundamentals/calculator/calculator.go
+++ project_1_go_fundamentals/calculator/calculator.go
@@ -1,7 +1,10 @@
 // Package calculator provides a library for // simple calculations in Go.
 package calculator
 
-import "fmt"
+import (
+	"errors"
+	//"fmt"
+)
 
 func Add(a, b float64) float64 {
 	return a + b
@@ -16,8 +19,13 @@ func Multiply(a, b float64) float64 {
 }
 
 func Divide(a, b float64) (float64, error) {
-	if b == 0{
-		return 0, fmt.Errorf("bad input: %f, %f (division by zero is undefined)", a, b)	
+	if b == 0 {
+		//Use errors.New() when I'm not using verbs to format a string. The test is only interested if the return value is Not nil. SO any message that's not nil would work here)
+		//Anything can take the place of the return float64 bc we're interested in the non-nil return vaue 
+		return 0, errors.New("Division by zero is undefined")
+		//return 0, fmt.Errorf("bad input: %f, %f (division by zero is undefined)", a, b)	
 	}
 	return a/b, nil
 }
+
+//If NOT nil, then an error happened 
--- project_1_go_fundamentals/calculator/calculator_test.go
+++ project_1_go_fundamentals/calculator/calculator_test.go
@@ -6,32 +6,35 @@ import (
 	"testing"
 )
 
-func TestAdd(t *testing.T) {
-	t.Parallel()
+// func TestAdd(t *testing.T) {
+// 	t.Parallel()
 
-	type testCase struct {
-        a, b float64
-        want float64
-    }
-	testCases := []testCase{
-		{ a: 2, b: 2, want: 4 },
-		{ a: 1, b: 1, want: 2 },
-		{ a: 5, b: 0, want: 5 },
-	}
+// 	type testCase struct {
+//         a, b float64
+//         want float64
+//     }
+// 	testCases := []testCase{
+// 		{ a: 2, b: 2, want: 4 },
+// 		{ a: 1, b: 1, want: 2 },
+// 		{ a: 5, b: 0, want: 5 },
+// 		{a: 0, b:0, want: 0},
+// 	}
 
-	for _, tc := range testCases {
-		got := calculator.Add(tc.a, tc.b)
-		if tc.want != got {
-			t.Errorf("want %f, got %f", tc.want, got)
-		} else if got > 0 {
-			fmt.Printf("\n%f plus %f sum to a positive number", tc.a, tc.b)
-		} else {
-			fmt.Printf("\n%f plus %f sum to a negative number", tc.a, tc.b)
-		}
+// 	for _, tc := range testCases {
+// 		got := calculator.Add(tc.a, tc.b)
+// 		if tc.want != got {
+// 			t.Errorf("want %f, got %f", tc.want, got)
+// 		} else if got > 0 {
+// 			fmt.Printf("\n%f plus %f sum to a positive number", tc.a, tc.b)
+// 		} else if got < 0 {
+// 			fmt.Printf("\n%f plus %f sum to a negative number", tc.a, tc.b)
+// 		} else {
+// 			fmt.Printf("\n%f minus % f is zero", tc.a, tc.b)
+// 		}
 
-	}
+// 	}
 
-}
+// }
 
 func TestSubtract(t *testing.T) {
 	t.Parallel()
@@ -44,6 +47,7 @@ func TestSubtract(t *testing.T) {
 	testCases := []testCase {
 		{a: 4, b: 2, want: 2},
 		{a: -6, b: 1, want: -7},
+		{a: 0, b: 0, want: 0},
 		//{a: 2.2, b: 1.5, want: 0.7},
 	}
 
@@ -51,6 +55,12 @@ func TestSubtract(t *testing.T) {
 		got := calculator.Subtract(tc.a, tc.b)
 		if tc.want != got {
 			t.Errorf("want %f, got %f", tc.want, got)
+		} else if got > 0 {
+			fmt.Printf("\n%f minus %f is a positive number", tc.a, tc.b)
+		} else if got < 0 {
+			fmt.Printf("\n%f minus %f is a negative number", tc.a, tc.b)
+		} else {
+			fmt.Printf("\n%f minus % f is zero", tc.a, tc.b)
 		}
 	}
 
@@ -74,6 +84,12 @@ func TestMultiply(t *testing.T) {
 		got := calculator.Multiply(tc.a, tc.b)
 		if tc.want != got {
 			t.Errorf("want %f, got %f", tc.want, got)
+		} else if got > 0{
+			fmt.Printf("\n%f multiplied by %f results in a positive number", tc.a, tc.b)
+		} else if got < 0{
+			fmt.Printf("\n%f multiplied by %f results in a negative number", tc.a, tc.b)
+		} else {
+			fmt.Printf("\n%f multiplied by %f is zero\n", tc.a, tc.b)
 		}
 
 	}
@@ -89,17 +105,46 @@ func TestDivision(t *testing.T) {
 	}
 
 	testCases := []testCase {
-		{a: 4, b: 2, want: 2, errExpected: false},
-		{a: 4, b: 0, want: 0, errExpected: true},
-		{a: 10, b: 10, want:1, errExpected: false},
+
+		{a: 3, b: 3, want:1, errExpected: false},
+		{a: 3, b: 0, want: 999, errExpected: true},
+		// //Use an unlikely value for "want" bc If I see this value then I know something went wrong with the test
+		// {a: 4, b: 0, want: 99, errExpected: true},
+	
 	}
+
 
 	for _, tc := range testCases {
 		got, err:= calculator.Divide(tc.a, tc.b)
-		if tc.want != got && err != nil {
+		//Did i rec an err? Was that err val nil or not?
+		//create a var to determine if there was an error, then is val is T. Otherwise there was NO error, so this val is false
+		errRecieved := (err != nil)
+
+		//IF the rec'd output doens't match my expectations, then raise an error. 
+		//I always need to check what the program returns against my expectatations
+		//This syntax checks 2 things: If there was an error and I didn't expect one OR I didn't get an error and I exptedted one. 
+		//So I'm looking to see if there's a mismatch between the error status that I rec'd (via: errReceived) and the error status that I expected (via: tc.errExpected)
+		if errRecieved != tc.errExpected {
+			//Don't move to any other tests
+			t.Fatalf("Divide (%.1f, %.1f): unexpected error status: %v ", tc.a, tc.b, err)	
+		} 
+
+		//alt: if !tc.errExpected && tc.want != got  -->same thing: checking my expectations
+		//Only want to compare want and got (via: && tc.want != got) when we don't expect an error
+		//Recall: if first val is T, then return it. Otherwise move on to the next value.
+		if err == nil && tc.want != got  {
 			t.Errorf("want %f, got %f", tc.want, got)	
 		}
-
+        
 	}	
 }
+
+//If just printing a message: t.Error() 
+//If printing w/values: t.Errorf()
+/*
+Don't move to any other tests:
+t.Fatalf("Divide (%.1f, %.1f): want error, got nil", tc.a, tc.b)	
+%v - prints out any val of any type. So I can use it to print the value of the error
+
+*/
 
