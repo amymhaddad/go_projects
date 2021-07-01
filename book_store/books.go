bda6efee7fd5c8fa3621f470b4193fdd5cc51f61
Amy M Haddad <amy.m.haddad@gmail.com>
2021-07-01 11:46:41 -0400

    Added netPrice function

--- book_store/books.go
+++ book_store/books.go
@@ -1,28 +1,39 @@
+/*
+Add suitable fields to the Book struct to represent a book that is a member of a series. For example, this book is the second in a series called “For the Love of Go”. How would you represent that? Your solution should make it possible to list a book series in order, for example.
+*/
+
 package main
 
-import "fmt"
-
-// func author() {
-// 	var authorName string
-// 	authorName = "Amy"
-// 	fmt.Println(authorName)
-// }
-
-func bookEdition(editionNum float64) float64 {
-	return editionNum
+type Book struct {
+	Name string
+	Edition int
+	Series string
+	Price int
+	DiscountPercent int
+	Pages string
 }
 
-func specialDiscount(isDiscount bool, amount float64) float64 {
-	if isDiscount {
-		return amount
-	} else {
-		return 0
-	}
+const centsPerDollar = 100
+
+func netPrice(price, discount int) int {
+	dollars := price / centsPerDollar
+	discountValue := float64(discount) * .10
+	newPrice := (float64(dollars) - discountValue) * 100
+
+	return int(newPrice)
 }
 
 func main() {
-	fmt.Println(bookEdition(1))
-	// x := author()
-	// fmt.Println(x)
-	fmt.Println(specialDiscount(true, 15))
+	book1 := Book {
+		Name: "Data", 
+		Edition: 2, 
+		Series: "For the love of go",
+		Price: 400, 
+		DiscountPercent: 2,
+	}
+	
+	updatedBookPrice := netPrice(book1.Price, book1.DiscountPercent)
+
 }
+
+
