55fd685755e3f45357bb08e87c5b987cee5a17d6
Amy M Haddad <amy.m.haddad@gmail.com>
2021-09-23 14:51:52 -0400

    Added multiple test cases to Add test

--- calculator/calculator_test.go
+++ calculator/calculator_test.go
@@ -7,12 +7,25 @@ import (
 
 func TestAdd(t *testing.T) {
 	t.Parallel()
-	var want float64 = 4
 
-	got := calculator.Add(2, 2)
-	if want != got {
-		t.Errorf("want %f, got %f", want, got)
+	type testCase struct {
+		a, b float64
+		want float64
 	}
+
+	testCases := []testCase{
+		{a: 2, b: 2, want: 4},
+		{a: 1, b: 2, want: 3},
+	}
+
+	for _, tc := range testCases {
+		got := calculator.Add(tc.a, tc.b)
+
+		if tc.want != got {
+			t.Errorf("want %f, got %f", tc.want, got)
+		}
+	}
+
 }
 
 func TestSubtract(t *testing.T) {
