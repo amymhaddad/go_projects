ff9f4d25197bf54c2e97f92ea7c0d39c250478f4
Amy M Haddad <amy.m.haddad@gmail.com>
2021-10-04 12:09:05 -0400

    Added functionality to get a single book

--- project_1_go_fundamentals/bookstore/bookstore.go
+++ project_1_go_fundamentals/bookstore/bookstore.go
@@ -11,7 +11,10 @@ type Book struct {
 	Title  string
 	Author string
 	Copies int
+	ID     int
 }
+
+var catalog map[int]Book
 
 //Buy reduces the copies of the book left
 func Buy(b Book) (Book, error) {
@@ -22,18 +25,33 @@ func Buy(b Book) (Book, error) {
 	return b, nil
 }
 
-func GetAllBooks(catalog []Book) []Book {
-	return catalog
-	//  b := []Book{}
-	// for _, books := range books {
-	// 	b = append(b, books)
-	// }
-	// return b
-} 
+//GetAllBooks returns all books in the catalog
+func GetAllBooks(catalog map[int]Book) []Book {
+	 
+	 b := []Book{}
+	for _, books := range catalog{
+		b = append(b, books)
+	}
+	return b
+}
+
+//GetBook returns a single book
+func GetBook(catalog map[int]Book, bookID int) (Book, error) {
+	//Using map as cache is much quicker than iteration
+
+	//Do this map check outside of if statement. That way I can still access the book value
+	book, found := catalog[bookID]
+
+	if !found {
+		return Book{}, errors.New("book doesn't exist")
+	}
+	return book, nil
+
+}
 
 // func main() {
 // 	want := []Book{
-// 		{Title:  "T1"}, 
+// 		{Title:  "T1"},
 // 		{Title:  "T2"},
 // 	}
 // 	x := GetAllBooks(want)
--- project_1_go_fundamentals/bookstore/bookstore_test.go
+++ project_1_go_fundamentals/bookstore/bookstore_test.go
@@ -21,14 +21,14 @@ func TestBook(t *testing.T) {
 func TestBuy(t *testing.T) {
 	t.Parallel()
 
-	//Create an instance of the type Book 
+	//Create an instance of the type Book
 	b := bookstore.Book{
 		Title:  "T1",
 		Author: "A1",
 		Copies: 2,
 	}
 
-	want := 1 
+	want := 1
 	results, err := bookstore.Buy(b)
 
 	if err != nil {
@@ -55,22 +55,55 @@ func TestBuyErrorsIfNoCopiesLeft(t *testing.T) {
 
 func TestGetAllBooks(t *testing.T) {
 	t.Parallel()
-	//Create my own slice of books to test 
-	catalog := []bookstore.Book{
-		{Title:  "T1"}, 
-		{Title:  "T2",},
-	}
 
+	catalog := map[int]bookstore.Book{
+		1: {Title: "T1", ID: 1},
+		2: {Title: "T2", ID: 2},
+	}
 	want := []bookstore.Book{
-		{Title:  "T1"}, 
-		{Title:  "T2"},
+		1: {Title: "T1", ID: 1},
+		2: {Title: "T2", ID: 2},
 	}
 
 	got := bookstore.GetAllBooks(catalog)
 
 	if !cmp.Equal(want, got) {
-		t.Error(cmp.Diff(want, got))
+		t.Errorf(cmp.Diff(want, got))
+	}
+}
+
+func TestGetBook(t *testing.T) {
+	t.Parallel()
+
+	catalog := map[int]bookstore.Book{
+		1: {Title: "T1", ID: 1},
+		2: {Title: "T2", ID: 2},
 	}
 
+	want := bookstore.Book{Title: "T2", ID: 2}
+
+	bookID := 2
+	got, err := bookstore.GetBook(catalog, bookID)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if want != got {
+		t.Errorf("want doesn't equal got")
+	}
+}
+
+func TestBookWithBadIDReturnsError(t *testing.T) {
+	t.Parallel()
+
+	catalog := map[int]bookstore.Book{}
+	bookID := 2
+	_, err := bookstore.GetBook(catalog, bookID)
+
+	//Checking if there's NO error (ie, I get a nil val back. If that's the case, return an error)
+	if err == nil {
+		t.Fatal("want an error for non-existing ID, but got nil")
+	}
 
 }
