466e6fe9c4fffa459e1a904589f45d407daae557
Amy M Haddad <amy.m.haddad@gmail.com>
2021-10-09 09:59:35 -0400

    Added second implementation of get book function

--- data_project/bookstore/bookstore.go
+++ data_project/bookstore/bookstore.go
@@ -1,7 +1,10 @@
 package bookstore
 
+import "fmt"
+
 //Book contains details about a book
 type Book struct {
+	ID     string
 	Title  string
 	Author string
 	Copies int
@@ -13,6 +16,17 @@ func GetBooks(catalog []Book) []Book {
 }
 
 //GetBookDetails gets the details for a particular book
-func GetBookDetails(catalog map[int]Book, ID int) Book {
-	return catalog[ID]
+// func GetBookDetails(catalog map[int]Book, ID int) Book {
+// 	return catalog[ID]
+// }
+
+//GetBookDetails gets the details for a particular book
+func GetBookDetails(catalog []Book, ID string) string {
+
+	for _, book := range catalog {
+		if book.ID == ID {
+			return fmt.Sprintf("%s, by %s", book.Title, book.Author)
+		}
+	}
+	return ""
 }
--- data_project/bookstore/bookstore_test.go
+++ data_project/bookstore/bookstore_test.go
@@ -2,10 +2,26 @@ package bookstore_test
 
 import (
 	"bookstore"
+	"fmt"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
 )
+
+var catalog = []bookstore.Book{
+	{
+		ID:     "1",
+		Title:  "Title 1",
+		Author: "A1",
+		Copies: 1,
+	},
+	{
+		ID:     "2",
+		Title:  "Title 2",
+		Author: "A2",
+		Copies: 1,
+	},
+}
 
 func TestBook(t *testing.T) {
 	t.Parallel()
@@ -19,43 +35,56 @@ func TestBook(t *testing.T) {
 func TestGetBooks(t *testing.T) {
 	t.Parallel()
 
-	catalog := []bookstore.Book{
-		{
-			Title:  "Title 1",
-			Author: "A1",
-			Copies: 1,
-		},
-		{
-			Title:  "Title 2",
-			Author: "A2",
-			Copies: 1,
-		},
-	}
+	// catalog := []bookstore.Book{
+	// 	{
+	// 		Title:  "Title 1",
+	// 		Author: "A1",
+	// 		Copies: 1,
+	// 	},
+	// 	{
+	// 		Title:  "Title 2",
+	// 		Author: "A2",
+	// 		Copies: 1,
+	// 	},
+	// }
 
 	want := catalog
 	got := bookstore.GetBooks(catalog)
 
 	if !cmp.Equal(want, got) {
-		cmp.Diff(want, got)
+		t.Errorf(cmp.Diff(want, got))
 	}
 
 }
 
 func TestGetBookDetails(t *testing.T) {
 	t.Parallel()
-	catalog := map[int]bookstore.Book{
-		1: {Title: "T1", Author: "A1", Copies: 1},
-		2: {Title: "T2", Author: "A2", Copies: 1},
-	}
 
-	ID := 2
+	want := "Title 1, by A1"
 
-	want := bookstore.Book{Title: "T1", Author: "A1", Copies: 1}
-
-	got := bookstore.GetBookDetails(catalog, ID)
-
+	got := bookstore.GetBookDetails(catalog, "1")
+	fmt.Println(got == want)
 	if !cmp.Equal(want, got) {
-		cmp.Diff(want, got)
+		t.Errorf(cmp.Diff(want, got))
 	}
 
 }
+
+// func TestGetBookDetails(t *testing.T) {
+// 	t.Parallel()
+// 	catalog := map[int]bookstore.Book{
+// 		1: {Title: "T1", Author: "A1", Copies: 1},
+// 		2: {Title: "T2", Author: "A2", Copies: 1},
+// 	}
+
+// 	ID := 2
+
+// 	want := bookstore.Book{Title: "T1", Author: "A1", Copies: 1}
+
+// 	got := bookstore.GetBookDetails(catalog, ID)
+
+// 	if !cmp.Equal(want, got) {
+// 		cmp.Diff(want, got)
+// 	}
+
+// }
