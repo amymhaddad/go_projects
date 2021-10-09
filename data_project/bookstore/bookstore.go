1d488cb10ae38252016a643a536cbf13dc80a679
Amy M Haddad <amy.m.haddad@gmail.com>
2021-10-09 10:45:52 -0400

    Refactoring

--- data_project/bookstore/bookstore.go
+++ data_project/bookstore/bookstore.go
@@ -1,32 +1,48 @@
 package bookstore
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 //Book contains details about a book
 type Book struct {
-	ID     string
+	ID     int
 	Title  string
 	Author string
 	Copies int
 }
 
+type Catalog map[int]Book
+
 //GetBooks returns a catalog of books
-func GetBooks(catalog []Book) []Book {
-	return catalog
-}
-
-//GetBookDetails gets the details for a particular book
-// func GetBookDetails(catalog map[int]Book, ID int) Book {
-// 	return catalog[ID]
-// }
-
-//GetBookDetails gets the details for a particular book
-func GetBookDetails(catalog []Book, ID string) string {
-
+//NOW that I have a Catalog type, I no longer need this long param declaration: func GetBooks(catalog map[int]Book) []Book {
+func GetBooks(catalog Catalog) []Book {
+	var allBooks []Book
 	for _, book := range catalog {
-		if book.ID == ID {
-			return fmt.Sprintf("%s, by %s", book.Title, book.Author)
-		}
+		allBooks = append(allBooks, book)
 	}
-	return ""
+	return allBooks
 }
+
+//GetBookDetails gets the details for a particular book
+func GetBookDetails(catalog Catalog, ID int) (string, error) {
+
+	book, found := catalog[ID]
+	if !found {
+		return "", errors.New("book isn't found")
+	}
+
+	return fmt.Sprintf("%s, by %s", book.Title, book.Author), nil
+}
+
+//GetBookDetails gets the details for a particular book
+// func GetBookDetails(catalog []Book, ID string) (string, error) {
+// 	for _, book := range catalog {
+// 		if book.ID == ID {
+// 			//Sprintf() returns a string instead of printing to terminal
+// 			return fmt.Sprintf("%s, by %s", book.Title, book.Author), nil
+// 		}
+// 	}
+// 	return "", errors.New("book isn't found")
+// }
--- data_project/bookstore/bookstore_test.go
+++ data_project/bookstore/bookstore_test.go
@@ -2,31 +2,24 @@ package bookstore_test
 
 import (
 	"bookstore"
-	"fmt"
+	"sort"
+
+	//"fmt"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
 )
 
-var catalog = []bookstore.Book{
-	{
-		ID:     "1",
-		Title:  "Title 1",
-		Author: "A1",
-		Copies: 1,
-	},
-	{
-		ID:     "2",
-		Title:  "Title 2",
-		Author: "A2",
-		Copies: 1,
-	},
+//Must use var for package level declarations, not the short declaration :=
+var catalog = bookstore.Catalog{
+	1: {ID: 1, Title: "T1", Author: "A1", Copies: 1},
+	2: {ID: 2, Title: "T2", Author: "A2", Copies: 1},
 }
 
 func TestBook(t *testing.T) {
 	t.Parallel()
 	_ = bookstore.Book{
-		Title:  "Title 1",
+		Title:  "T1",
 		Author: "A1",
 		Copies: 1,
 	}
@@ -35,22 +28,16 @@ func TestBook(t *testing.T) {
 func TestGetBooks(t *testing.T) {
 	t.Parallel()
 
-	// catalog := []bookstore.Book{
-	// 	{
-	// 		Title:  "Title 1",
-	// 		Author: "A1",
-	// 		Copies: 1,
-	// 	},
-	// 	{
-	// 		Title:  "Title 2",
-	// 		Author: "A2",
-	// 		Copies: 1,
-	// 	},
-	// }
+	want := []bookstore.Book{
+		{ID: 1, Title: "T1", Author: "A1", Copies: 1},
+		{ID: 2, Title: "T2", Author: "A2", Copies: 1},
+	}
 
-	want := catalog
 	got := bookstore.GetBooks(catalog)
 
+	sort.Slice(got, func(i, j int) bool {
+		return got[i].ID < got[j].ID
+	})
 	if !cmp.Equal(want, got) {
 		t.Errorf(cmp.Diff(want, got))
 	}
@@ -60,31 +47,45 @@ func TestGetBooks(t *testing.T) {
 func TestGetBookDetails(t *testing.T) {
 	t.Parallel()
 
-	want := "Title 1, by A1"
+	ID := 2
 
-	got := bookstore.GetBookDetails(catalog, "1")
-	fmt.Println(got == want)
+	want := "T1, by A1"
+
+	got, err := bookstore.GetBookDetails(catalog, ID)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
 	if !cmp.Equal(want, got) {
-		t.Errorf(cmp.Diff(want, got))
+		cmp.Diff(want, got)
 	}
 
 }
 
+//Unsure why this test doesn't work when I try an ID I know that won't work?
+//Test error values w/separate test?
 // func TestGetBookDetails(t *testing.T) {
 // 	t.Parallel()
-// 	catalog := map[int]bookstore.Book{
-// 		1: {Title: "T1", Author: "A1", Copies: 1},
-// 		2: {Title: "T2", Author: "A2", Copies: 1},
+
+// 	want := "Title 1, by A1"
+// 	got, err := bookstore.GetBookDetails(catalog, "3")
+
+// 	if err != nil {
+// 		t.Fatal(err)
 // 	}
-
-// 	ID := 2
-
-// 	want := bookstore.Book{Title: "T1", Author: "A1", Copies: 1}
-
-// 	got := bookstore.GetBookDetails(catalog, ID)
 
 // 	if !cmp.Equal(want, got) {
-// 		cmp.Diff(want, got)
+// 		t.Errorf(cmp.Diff(want, got))
 // 	}
 
+// }
+//This is the function:
+// func GetBookDetails(catalog []Book, ID string) (string, error) {
+// 	for _, book := range catalog {
+// 		if book.ID == ID {
+// 			return fmt.Sprintf("%s, by %s", book.Title, book.Author), nil
+// 		}
+// 	}
+// 	return "", errors.New("book isn't found")
 // }
